00b28fdbc90a15a2ed21e44391d737e2bf18aafc
Gabriel Trésor TEK's <gabrielsuprematie@gmail.com>
2019-01-30 09:28:42 +0100

    Add register page and github info

--- components/RegisterPage.js
+++ components/RegisterPage.js
@@ -1,35 +1,149 @@
 import React, { Component } from 'react';
-import { Text, View, StyleSheet, Image } from 'react-native';
+import { Text, View, StyleSheet, Image, TextInput, TouchableOpacity, Switch } from 'react-native';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 
 export default class RegisterPage extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            switch1Value: false,
+        }
+    }
+
+    toggleSwitch1 = (value) => {
+        this.setState({ switch1Value: value })
+        console.log('Switch 1 is: ' + value)
+    }
+
     render() {
         return (
-            <View style={styles.container}>
-                <Text style={styles.paragraph}>
-                    Register
-        </Text>
-                <Image style={styles.logo} source={require("../assets/icon.png")} />
+            <View
+                style={{
+                    flex: 1,
+                    backgroundColor: '#eee',
+                }}
+            >
+
+                <View
+                    style={{
+                        position: 'absolute',
+                        top: 0,
+                        left: 0,
+                        width: '100%',
+                        height: '100%',
+                    }}
+                >
+
+                    <Image
+                        style={{
+                            flex: 1,
+                            resizeMode: 'cover',
+                        }}
+                        source={require('../assets/bg_app.png')}
+                    />
+
+                </View>
+
+
+                <View
+                    style={{
+                        flex: 1,
+                        backgroundColor: 'transparent',
+                        justifyContent: 'center',
+                    }}
+                >
+                    <KeyboardAwareScrollView>
+                        <View style={{ justifyContent: 'center', alignItems: 'center' }}>
+                            <Image source={require('../assets/icon.png')} style={styles.image} />
+
+                            <Text
+                                style={{
+                                    textAlign: 'center',
+                                    fontSize: 22,
+                                }}
+                            >
+                                Veuillez vous enregister s'il vous plait
+                            </Text>
+                        </View>
+
+                        <View style={styles.main}>
+                            <Text>Nom complet</Text>
+                            <TextInput underlineColorAndroid='transparent' style={styles.input} />
+
+                            <Text>Email</Text>
+                            <TextInput underlineColorAndroid='transparent' style={styles.input} />
+
+                            <Text>Mot de passe</Text>
+                            <TextInput underlineColorAndroid='transparent' style={styles.input} secureTextEntry={true} />
+                            <View style={{ flexDirection: 'row', alignItems: 'flex-start' }}>
+
+                                <Switch onValueChange={this.toggleSwitch1} onTintColor="#fcc358"
+                                    value={this.state.switch1Value} />
+                                <Text style={{ alignSelf: 'center', textAlign: 'left' }}>J'accepte les termes et conditions</Text>
+
+                            </View>
+                            <TouchableOpacity style={styles.buttonContainer}>
+                                <Text style={styles.buttonText}> VALIDER </Text>
+                            </TouchableOpacity>
+
+                        </View>
+                    </KeyboardAwareScrollView>
+                </View>
+
             </View>
         );
     }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        alignItems: 'center',
-        justifyContent: 'center',
+    input: {
+        height: 45,
+        borderColor: 'gray',
+        borderWidth: 1,
+        borderRadius: 5,
+        marginBottom: 20,
+        fontSize: 20,
+        paddingLeft: 5,
+        paddingRight: 5,
+        backgroundColor: '#FFFFFF',
     },
-    paragraph: {
-        margin: 24,
-        marginTop: 0,
-        fontSize: 14,
+    main: {
+        margin: 20,
+    },
+    image: {
+        marginBottom: 20,
+        marginTop: 50,
+        height: 80,
+        width: 80,
+
+    },
+
+    buttonContainer: {
+        backgroundColor: '#fcc358',
+        paddingVertical: 10,
+        marginTop: 20,
+        height: 50,
+        borderRadius: 5
+    },
+
+    buttonText: {
+        fontSize: 18,
         fontWeight: 'bold',
         textAlign: 'center',
-        color: '#34495e',
+        color: '#FFFFFF'
     },
-    logo: {
-        backgroundColor: "#056ecf",
-        height: 128,
-        width: 128,
-    }
+    footer: {
+        height: 30,
+        color: '#FFFFFF',
+        textAlign: 'center',
+        fontSize: 18,
+        backgroundColor: 'gray',
+
+    },
+    copyright: {
+        textAlign: 'center',
+        margin: 20,
+        fontSize: 14,
+    },
 });
